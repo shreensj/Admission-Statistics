cdc34c72c40c796047a4ca4a069cc3931e1f382e
shreensj <shree.nsj@gmail.com>
2026-04-12 19:22:38 +0530

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,8 @@
+window.FIREBASE_CONFIG = {
+  apiKey: "AIzaSyBY8tKhGISwfjauUOCosKvIdq4I_-saoJg",
+  authDomain: "add-web-app-5ea10.firebaseapp.com",
+  projectId: "add-web-app-5ea10",
+  storageBucket: "add-web-app-5ea10.firebasestorage.app",
+  messagingSenderId: "758424929679",
+  appId: "1:758424929679:web:b9a9e57f84c42910aa37fa",
+};
